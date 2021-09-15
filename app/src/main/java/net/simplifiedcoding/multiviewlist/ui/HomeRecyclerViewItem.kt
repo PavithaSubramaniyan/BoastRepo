873bfc9f27b1f502cd80c0eea89316c83858aace
PavithaSubramaniyan <srpavi13@gmail.com>
2021-09-15 10:08:04 +0530

    initial commit

---
+++ app/src/main/java/net/simplifiedcoding/multiviewlist/ui/HomeRecyclerViewItem.kt
@@ -0,0 +1,25 @@
+package net.simplifiedcoding.multiviewlist.ui
+
+sealed class HomeRecyclerViewItem {
+
+    class Title(
+        val id: Int,
+        val title: String
+    ) : HomeRecyclerViewItem()
+
+    class Movie(
+        val id: Int,
+        val title: String,
+        val thumbnail: String,
+        val release_date: String
+    ) : HomeRecyclerViewItem()
+
+    class Director(
+        val id: Int,
+        val name: String,
+        val avatar: String,
+        val title : String,
+        val movie_count: Int
+    ) : HomeRecyclerViewItem()
+
+}
---
+++ app/src/main/java/net/simplifiedcoding/multiviewlist/ui/HomeViewModel.kt
@@ -0,0 +1,49 @@
+package net.simplifiedcoding.multiviewlist.ui
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import net.simplifiedcoding.multiviewlist.data.network.Resource
+import net.simplifiedcoding.multiviewlist.data.repository.Repository
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val repository: Repository
+) : ViewModel() {
+
+    private val _homeListItemsLiveData = MutableLiveData<Resource<List<HomeRecyclerViewItem>>>()
+    val homeListItemsLiveData: LiveData<Resource<List<HomeRecyclerViewItem>>>
+        get() = _homeListItemsLiveData
+
+    init {
+        getHomeListItems()
+    }
+
+    private fun getHomeListItems() = viewModelScope.launch {
+        _homeListItemsLiveData.postValue(Resource.Loading)
+        val moviesDeferred = async { repository.getMovies() }
+        val directorsDeferred = async { repository.getDirectors() }
+
+        val movies = moviesDeferred.await()
+        val directors = directorsDeferred.await()
+
+        val homeItemsList = mutableListOf<HomeRecyclerViewItem>()
+        if(movies is Resource.Success && directors is Resource.Success){
+            homeItemsList.add(HomeRecyclerViewItem.Title(1, "Recommended Movies"))
+            homeItemsList.addAll(movies.value)
+            homeItemsList.add(HomeRecyclerViewItem.Title(2, "Top Directors"))
+            homeItemsList.addAll(directors.value)
+            _homeListItemsLiveData.postValue(Resource.Success(homeItemsList))
+            Log.i("MYTAG", "success")
+        }else{
+            Resource.Failure(false, null, null)
+            Log.i("MYTAG", "failure")
+        }
+    }
+}
